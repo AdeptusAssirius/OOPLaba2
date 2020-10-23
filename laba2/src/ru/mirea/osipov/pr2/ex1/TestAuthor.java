5fd94eab7485309b4232528f68dfbf4b25152db0
AlexanderBit-Shamash <73355733+AlexanderBit-Shamash@users.noreply.github.com>
2020-10-23 22:25:10 +0300

    Add files via upload

---
+++ laba2/src/ru/mirea/osipov/pr2/ex1/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.osipov.pr2.ex1;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Shar", "shar@yandex.ru", 'M');
+        System.out.println(a1);
+    }
+}
